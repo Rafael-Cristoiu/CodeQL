9f1f8753696ef00bed8534c3bbeaa49853aa31e0
Rafael Estefan <rafaelestefan@MacBook-Air-de-Rafael.local>
2024-09-13 22:32:10 -0300

    Remove shell

--- express.ts
+++ express.ts
@@ -1,16 +1,16 @@
-// import * as express from 'express';
+import * as express from 'express';
 
-// const app = express();
-// const port = 3000;
+const app = express();
+const port = 3000;
 
-// app.use(express.json());
+app.use(express.json());
 
-// // Função vulnerável a XSS
-// app.get('/greet', (req, res) => {
-//   const name = req.query.name as string; // Nome recebido da query string
-//   res.send(`<h1>Hello, ${name}!</h1>`); // Não sanitiza a entrada do usuário
-// });
+// Função vulnerável a XSS
+app.get('/greet', (req, res) => {
+  const name = req.query.name as string; // Nome recebido da query string
+  res.send(`<h1>Hello, ${name}!</h1>`); // Não sanitiza a entrada do usuário
+});
 
-// app.listen(port, () => {
-//   console.log(`Server is running at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
