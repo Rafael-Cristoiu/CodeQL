965e72e17040ac5ae77034ed6a2e7216455167f9
Rafael Estefan <rafaelestefan@MacBook-Air-de-Rafael.local>
2024-09-13 21:05:27 -0300

    Add hardcoded_example

---
+++ hardcoded_example.ts
@@ -0,0 +1,5 @@
+function connectToDatabase(): void {
+    const password: string = "s3cr3tP@ssw0rd";
+    console.log("Conectando ao banco de dados com a senha:", password);
+};
+connectToDatabase();
