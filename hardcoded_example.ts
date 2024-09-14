034aa9efc392dbf28821581e646458bfb82a724e
Rafael Estefan <rafaelestefan@MacBook-Air-de-Rafael.local>
2024-09-13 21:14:02 -0300

    Update

--- hardcoded_example.ts
+++ hardcoded_example.ts
@@ -1,5 +1,6 @@
-function connectToDatabase(): void {
-    const password: string = "s3cr3tP@ssw0rd";
-    console.log("Conectando ao banco de dados com a senha:", password);
-};
-connectToDatabase();
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'test'
+});
