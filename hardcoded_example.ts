a2ffed4de5e37973969656dfa1fe60fc9c8bc4d3
Rafael Estefan <rafaelestefan@MacBook-Air-de-Rafael.local>
2024-09-13 21:19:34 -0300

    Update ts

--- hardcoded_example.ts
+++ hardcoded_example.ts
@@ -1,6 +1,17 @@
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'test'
-});
+class ApiClient {
+    private readonly apiKey: string = "12345-abcde-67890-fghij";
+    private readonly apiSecret: string = "s3cr3tKeyForAPI!"; 
+
+    public connect(): void {
+        console.log("Conectando ao serviço com:");
+        console.log(`Chave de API: ${this.apiKey}`);
+        console.log(`Segredo da API: ${this.apiSecret}`);
+     
+    }
+}
+function main(): void {
+    const client = new ApiClient();
+    client.connect();
+}
+
+main();
